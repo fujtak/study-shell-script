5393d7b760df8053241f97cd72ad6b1adcd349eb
fujtak <fujtak.eng23@gmail.com>
2025-02-02 20:13:03 +0900

    メインルーチンを関数に切り出し

--- Appendix/progressbar.sh
+++ Appendix/progressbar.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-i=0
-while [ $i -le 100 ]
-do
-  echo "ループ${i}回目"
-  sleep 1
-  i=$(expr $i + 1)
-done
+main()
+{
+  i=0
+  while [ $i -le 100 ]
+  do
+    echo "ループ${i}回目"
+    sleep 1
+    i=$(expr $i + 1)
+  done
+}
+
+main
