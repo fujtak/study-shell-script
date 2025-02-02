1f24cc5698f3154b3c4a013091ee638a3b67d5a9
fujtak <fujtak.eng23@gmail.com>
2025-02-03 00:05:19 +0900

    プログレスバーを実装

--- Appendix/progressbar.sh
+++ Appendix/progressbar.sh
@@ -1,14 +1,47 @@
-#!/bin/sh
+#!/bin/bash
+
+print_bar()
+(
+  percent=$1
+
+  width_indicator=71
+  width_arrow=`expr $width_indicator \* $percent / 100`
+  width_space=`expr $width_indicator - $width_arrow`
+
+  bar='\r['
+
+  i=0
+  while [ $i -lt $width_arrow ]
+  do
+    bar=$bar'='
+    i=`expr $i + 1`
+  done
+  bar="$bar>"
+
+  j=0
+  while [ $j -lt $width_space ]
+  do
+    bar=$bar'_'
+    j=`expr $j + 1`
+  done
+
+  bar="$bar] $percent%\c"
+
+  echo -e $bar
+)
 
 main()
 {
+  # 擬似的に進捗の%を再現
   i=0
   while [ $i -le 100 ]
   do
-    echo "ループ${i}回目"
-    sleep 1
-    i=$(expr $i + 1)
+    print_bar $i
+    sleep 0.05
+    i=`expr $i + 1`
   done
+  echo -e "\r"
+  echo 'completed'
 }
 
 main
