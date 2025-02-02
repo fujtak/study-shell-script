adcc61e043888b2fe5e586806591615c7c0cf522
fujtak <fujtak.eng23@gmail.com>
2025-02-03 01:07:22 +0900

    作成中

--- project/copyDistToPub.sh
+++ project/copyDistToPub.sh
@@ -6,6 +6,34 @@ print_error()
   printf "\033[31m%s\033[m\n" $message
 )
 
+print_bar()
+(
+  percent=$1
+
+  width_indicator=71
+  width_arrow=`expr $width_indicator \* $percent / 100`
+  width_space=`expr $width_indicator - $width_arrow`
+
+  bar='\r['
+
+  i=0
+  while [ $i -lt $width_arrow ]; do
+    bar=$bar'='
+    i=`expr $i + 1`
+  done
+  bar="$bar>"
+
+  j=0
+  while [ $j -lt $width_space ]; do
+    bar=$bar'_'
+    j=`expr $j + 1`
+  done
+
+  bar="$bar] $percent%\c"
+
+  echo -e $bar
+)
+
 main()
 (
   if ! [ -d "./dist/" ]; then
@@ -16,10 +44,19 @@ main()
     exit 1
   fi
 
+  count_files=`find ./dist/ -type f | wc -l`
+
+  i=0
   for file in './dist/'*
   do
     cp -r $file './target/'
+    percent=`expr $i / $count_files \* 100`
+    print_bar $percent
+    i=`expr $i + 1` # increment
   done
+
+  echo -e "\r"
+  echo 'completed'
 )
 
 main
